e5f6877eba0654f9c2aa1bd356aa48d4c35bf9af
kariya <sgkuarut@yahoo.co.jp>
2019-10-20 12:19:40 +0000

    text21

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 mx-suto">
+            <div class="col-md-8 mx-auto">
                 <h2>ニュース新規作成</h2>
             </div>
         </div>
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 mx-suto">
+            <div class="col-md-8 mx-auto">
                 <h2>応用プロフィール</h2>
             </div>
         </div>
